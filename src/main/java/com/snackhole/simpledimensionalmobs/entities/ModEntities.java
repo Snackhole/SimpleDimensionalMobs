28dc72a7506a14248a4447ae56f5548749d69e35
Snackhole <>
2018-03-24 21:13:47 -0500

    ModEntities class takes hex strings for egg colors

--- src/main/java/com/snackhole/simpledimensionalmobs/entities/ModEntities.java
+++ src/main/java/com/snackhole/simpledimensionalmobs/entities/ModEntities.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModEntities {
     public static void init() {
         int id = 1;
-        registerMob(EntityMagmaLurcher.class, "magma_lurcher", id++, 6958606, 16293444, Biomes.HELL);
+        registerMob(EntityMagmaLurcher.class, "magma_lurcher", id++, "6a2e0e", "f89e44", Biomes.HELL);
     }
 
     @SideOnly(Side.CLIENT)
@@ -24,8 +24,8 @@ public class ModEntities {
         RenderingRegistry.registerEntityRenderingHandler(EntityMagmaLurcher.class, RenderMagmaLurcher.FACTORY);
     }
 
-    private static void registerMob(Class<? extends EntityLiving> entityClass, String name, int id, int eggPrimary, int eggSecondary, Biome... biomes) {
-        EntityRegistry.registerModEntity(new ResourceLocation(SimpleDimensionalMobsMain.MODID + ":" + name), entityClass, name, id, SimpleDimensionalMobsMain.simpleDimensionalMobsMainInstance, 64, 3, true, eggPrimary, eggSecondary);
+    private static void registerMob(Class<? extends EntityLiving> entityClass, String name, int id, String eggPrimary, String eggSecondary, Biome... biomes) {
+        EntityRegistry.registerModEntity(new ResourceLocation(SimpleDimensionalMobsMain.MODID + ":" + name), entityClass, name, id, SimpleDimensionalMobsMain.simpleDimensionalMobsMainInstance, 64, 3, true, Integer.parseInt(eggPrimary, 16), Integer.parseInt(eggSecondary, 16));
         EntityRegistry.addSpawn(entityClass, 100, 3, 5, EnumCreatureType.MONSTER, biomes);
     }
 }
