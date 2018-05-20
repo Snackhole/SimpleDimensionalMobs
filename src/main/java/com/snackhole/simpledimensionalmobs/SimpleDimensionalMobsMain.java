f78b529663831457d6798a047e395660ab930e5c
Snackhole <>
2018-05-20 12:55:20 -0500

    Bump version

--- src/main/java/com/snackhole/simpledimensionalmobs/SimpleDimensionalMobsMain.java
+++ src/main/java/com/snackhole/simpledimensionalmobs/SimpleDimensionalMobsMain.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = SimpleDimensionalMobsMain.MODID, version = SimpleDimensionalMobsMain.VERSION)
 public class SimpleDimensionalMobsMain {
     public static final String MODID = "simpledimensionalmobs";
-    public static final String VERSION = "1";
+    public static final String VERSION = "2";
     @SidedProxy(clientSide = "com.snackhole.simpledimensionalmobs.proxy.ClientProxy", serverSide = "com.snackhole.simpledimensionalmobs.proxy.ServerProxy")
     public static IProxy proxy;
     @Mod.Instance
